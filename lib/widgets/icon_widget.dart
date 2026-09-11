1060624cd867f321c47d4a2ae085c02df025fcca
husen <iamhusen101@gmail.com>
2026-09-11 16:16:29 +0530

    add icon widget, add HugeIcons for that

---
+++ lib/widgets/icon_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hugeicons/hugeicons.dart';
+
+typedef HugeIconsIconType = List<List<int>>;
+
+/// Wrapper for icons, it is useful when we want to use multiple types of icons
+/// in a project, like HugeIcons, FontAwesomeIcons etc.
+class IconWidget extends StatelessWidget {
+  const IconWidget({
+    super.key,
+    required this.icon,
+    this.size =22,
+    this.color,
+  });
+
+  final dynamic icon;
+  final double size;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    if (icon is HugeIconsIconType) {
+      return SizedBox(
+        width: size,
+        height: size,
+        child: HugeIcon(icon: icon, color: color),
+      );
+    } else if (icon is FaIconData) {
+      return FaIcon(icon, size: size, color: color);
+    } else {
+      return Icon(icon, size: size, color: color);
+    }
+  }
+}
